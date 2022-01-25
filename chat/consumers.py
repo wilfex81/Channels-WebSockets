f2c36dcd699c32a68f8697cfcf601cf60b8e2f80
wilfex <wilfex81@gmail.com>
2022-01-25 12:34:47 +0300

    First Commit

---
+++ chat/consumers.py
@@ -0,0 +1,39 @@
+from audioop import add
+from cgitb import text
+from email import message
+import json
+from tokenize import group
+from channels.generic.websocket import WebsocketConsumer
+from asgiref.sync import async_to_sync
+
+class ChatConsumer(WebsocketConsumer):
+    def connect(self):
+         self.room_group_name = 'test'   
+         async_to_sync(self.channel_layer.group_add)(
+             self.room_group_name,
+             self.channel_name
+         )
+         
+        
+        
+    def receive(self, text_data):
+        text_data_json = json.loads(text_data)
+        message = text_data_json['message'] 
+        
+        async_to_sync(self.channel_layer.group_send)(
+            self.room_group_name,
+            {
+                'type': 'chat_message',
+                'message': message
+            }
+        )
+    
+    
+    def chat_message(self, event):
+        message = event['message']
+        self.send(text_data = json.dumps({
+            'type': 'chat',
+            'message': message
+        }))
+        
+        
